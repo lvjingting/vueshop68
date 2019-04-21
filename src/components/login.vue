cc29520aabf4b892be1cffd3f8f06c93f71ad0cc
lv <573344042@qq.com>
2019-04-21 22:33:11 +0800

    login初始化

---
+++ src/components/login.vue
@@ -0,0 +1,12 @@
+<template>
+   <div>登录员管理系统</div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import Login from '@/components/Login'
+import '../assets/css/global.css'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    { path: '/login', component: Login },
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    }
+  ]
+})
